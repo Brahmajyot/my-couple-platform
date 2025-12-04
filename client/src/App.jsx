42d23a1ee13230534a83432fc3e138d6ef71670d
Brahmajyoti <brahmajyotinayak100@gmail.com>
2025-12-05 03:55:16 +0530

    app.jsx missing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute'; 
-import StreamProvider from './components/StreamProvider'; 
+import Home from './pages/Home.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx'; 
+import StreamProvider from './components/StreamProvider.jsx'; 
 
 function App() {
   return (
